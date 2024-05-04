694422602fdb714370dd95c6e965766af83ad930
nick <nicholas.diblasio@gmail.com>
2024-05-04 12:58:28 -0400

    Initial Commit

---
+++ vinylSync/vinylSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  vinylSyncApp.swift
+//  vinylSync
+//
+//  Created by Nick DiBlasio on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct vinylSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
