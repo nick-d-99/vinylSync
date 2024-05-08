bd7bf05eee2807269ed82ef744b876b601e3680e
nick <nicholas.diblasio@gmail.com>
2024-05-07 20:24:31 -0400

    Rearranged app layout and added support to display the album cover of the matched song.

--- vinylSync/ContentView.swift
+++ vinylSync/ContentView.swift
@@ -12,10 +12,13 @@ import Combine
 struct ContentView: View {
     @State var foundSong = "None"
     @State var artist = "None"
-    @State var background = Color.mint
+//    @State var background = Color.mint
+//    @State var background = Image("mint")
+    @State var background = UIImage(named: "mint")
+    @State var cover = UIImage(named:"defaultAlbum")
     
-    @State var header = "Press the vinyl to search for a song!"
-    @State var footer = "No song yet!"
+    @State var bottom = "Press the vinyl to search for a song!"
+    @State var top = "No song yet!"
 
     //    @State var searching = false
     
@@ -23,29 +26,38 @@ struct ContentView: View {
     @State var rotationAngle: Double = 0
     
     var body: some View {
+        
         ZStack{
-//            Color(.systemMint).ignoresSafeArea()
-            Color(background).ignoresSafeArea()
+            Image(uiImage:background!).resizable(resizingMode: .stretch).ignoresSafeArea()
             
             VStack{
-                ZStack{
-                    Text(header)
-                        .foregroundColor(Color.black)
+                
+                HStack{
+                    Image(uiImage: cover!).resizable().aspectRatio(contentMode: .fit).padding(.all, 30.0).cornerRadius(15)
                 }
-                .font(.title)
-                .padding()
-                .background(Rectangle())
-                .foregroundColor(.white)
-                .cornerRadius(15)
-                .shadow(radius: 15)
+                
+                ZStack{
+                    
+                    VStack{
+                        Text(top)
+                            .font(.title)
+                            .multilineTextAlignment(.center)
+                    }
+                    .padding()
+                    .background(Rectangle()
+                        .cornerRadius(15)
+                        .foregroundColor(.white)
+                        .shadow(radius: 15))
+
+                }
                 
                 Button {
                     Task {
                         withAnimation {
                             isSpinning = true // Start spinning animation
                         }
-                        header = "Searching..."
-                        footer = "Finding a match..."
+                        bottom = "Searching..."
+                        top = "Finding a match..."
                         await findSong()
                         //                        found = "Song found!"
                         
@@ -59,7 +71,7 @@ struct ContentView: View {
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .mask(
-                            Circle().frame(width: 195, height: 250)
+                            Circle().frame(width: 145, height: 250)
                         )
                         .rotationEffect(.degrees(isSpinning ? rotationAngle : 0)) // Rotate image when spinning
                 }
@@ -72,23 +84,15 @@ struct ContentView: View {
                 }
                 
                 ZStack{
-                    VStack{
-                        //                        Text("Song found: ")
-                        //                            .font(.title)
-//                        Text("\(foundSong) by ")
-                        Text(footer)
-                            .font(.title)
-                            .multilineTextAlignment(.center)
-//                        Text(artist)
-//                            .font(.title)
-                    }
-                    .padding()
-                    .background(Rectangle()
-                        .cornerRadius(15)
-                        .foregroundColor(.white)
-                        .shadow(radius: 15))
-
+                    Text(bottom)
+                        .foregroundColor(Color.black)
                 }
+                .font(.title)
+                .padding()
+                .background(Rectangle())
+                .foregroundColor(.white)
+                .cornerRadius(15)
+                .shadow(radius: 15)
 
             }
             
@@ -103,22 +107,67 @@ struct ContentView: View {
               
         switch result {
          case .match(let match):
-                print("Match found. MediaItemsCount: \(match.mediaItems.count)")
-            foundSong = (match.mediaItems.first?.title)!
-            artist = (match.mediaItems.first?.artist)!
-            header = "Song found!"
-            footer = "\(foundSong) by: \n\(artist)"
+            let song = match.mediaItems.first
+
+            //                print("Match found. MediaItemsCount: \(match.mediaItems.count)")
+            
+            print("Match found. MediaItemsCount: \(match.mediaItems.count)")
+            
+            
+//            foundSong = (match.mediaItems.first?.title)!
+            
+            //need to correctly unwrap optional
+            //maybe optional binding?
+            foundSong = (song?.title)!
+            
+//            artist = (match.mediaItems.first?.artist)!
+            artist = (song?.artist)!
+            
+            bottom = "Song found!"
+            
+            
+            top = "\(foundSong) by: \n\(artist)"
+            
+            //test to see what parameter needs to be
+            //passed to the getURL function
+//            print(type(of: song?.artworkURL))
+            await getUrl(artworkURL: (song?.artworkURL)!)
+            
          case .noMatch(_):
-                header = "No match found :("
+                bottom = "No match found :("
                 print("No match found")
          case .error(_, _):
-                header = "An error occured :("
+                bottom = "An error occured :("
                 print("An error occurred")
         }
     }
     
-    func getUrl(url: String) async {
-        
+    func getUrl(artworkURL: URL) async {
+
+        let url = artworkURL
+//        print(url)
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+           if let error = error {
+
+              // handle error
+               print(error)
+               bottom = "URL error"
+               top = "URL error"
+              return
+           }
+           guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+              // handle error
+               bottom = "URL error"
+               top = "URL error"
+              return
+           }
+           if let data = data {
+              // process data
+//               print(type(of: data))
+               cover = (UIImage(data: data))
+           }
+        }
+        task.resume()
     }
 }
 
